5160ef58b9398042baa30a424c60a8b713e7694b
alamineGithub <mamadousaliouba96@gmail.com>
2024-09-16 02:16:50 +0000

    Add CI/CD workflow configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configurationSchema } from './config/dbschema';
+// import { configurationSchema } from './config/dbschema';
+import { Book } from './books/entities/book.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: configurationSchema, // Ajoutez le schéma de validation ici
+      // validationSchema: configurationSchema, // Ajoutez le schéma de validation ici
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [],
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [Book],
         synchronize: true,
       }),
       inject: [ConfigService],
