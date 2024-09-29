ced3d8f22e6450f7a7696d44d879b9d986151f30
alamineGithub <mamadousaliouba96@gmail.com>
2024-09-29 04:29:46 +0000

    deployment backend change env

--- src/config/dbschema.ts
+++ src/config/dbschema.ts
@@ -4,7 +4,7 @@ export const configurationSchema = Joi.object({
   DATABASE_HOST: Joi.string().default('mysql'),
   DATABASE_PORT: Joi.number().default(3306),
   DATABASE_USER: Joi.string().default('root'),
-  DATABASE_PASSWORD: Joi.string().default(''),
+  DATABASE_PASSWORD: Joi.string().default('root'),
   DATABASE_NAME: Joi.string().default('nestjs_books'),
   PORT: Joi.number().default(3001),
 });
